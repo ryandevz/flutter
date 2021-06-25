e7cf474646f620565bc5471a9e2bcd2826a36f55
Ryan Thomas <ryandev@protonmail.com>
2021-06-25 16:20:08 +0600

    I am Rich

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,16 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Ryandev'),
-      ),
-    ),
+        home: Scaffold(
+            backgroundColor: Colors.blueGrey.shade500,
+            appBar: AppBar(
+              title: Text('I Am Rich'),
+              backgroundColor: Colors.blueGrey.shade900,
+            ),
+            body: Center(
+              child: Image(
+                image: AssetImage('images/diamond.png'),
+              ),
+            ))),
   );
 }
